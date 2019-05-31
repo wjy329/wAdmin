27dc8aa070213c721d2efdb5cc9e3f38c6e446c0
wjy329@vip.qq.com <wjyshr6633153>
2019-05-31 15:47:12 +0800

    1.添加默认首页 2.替换layui-2.5.0

--- w-web/src/main/java/com/wjy329/WWebApplication.java
+++ w-web/src/main/java/com/wjy329/WWebApplication.java
@@ -7,6 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 
 @SpringBootApplication
@@ -17,6 +21,23 @@ public class WWebApplication extends SpringBootServletInitializer {
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
 		return builder.sources(WWebApplication.class);
+	}
+
+
+	@Configuration
+	public class DefaultView extends WebMvcConfigurerAdapter {
+
+		@Override
+		public void addViewControllers(ViewControllerRegistry registry) {
+			// 设定首页为index
+			registry.addViewController("/").setViewName("forward:/index");
+
+			// 设定匹配的优先级
+			registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
+
+			// 添加视图控制类
+			super.addViewControllers(registry);
+		}
 	}
 
 	public static void main(String[] args) {
